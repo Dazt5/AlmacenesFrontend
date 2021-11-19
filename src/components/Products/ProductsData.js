88a660ec21c20c250fa2bf0991bce44e1668f14f
David Alvarez <david.alvarez@valid.com>
2021-11-18 22:51:33 -0500

    ft: added product component

---
+++ src/components/Products/ProductsData.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export const ProductsData = [
+    {
+        codigo_producto: '1',
+        nombre_producto: 'Producto #1',
+        nitproveedor: '123',
+        precio_compra: '3500',
+        ivacompra: '8%',
+        precio_venta:'3800'
+    },
+    {
+        codigo_producto: '2',
+        nombre_producto: 'Producto #2',
+        nitproveedor: '456',
+        precio_compra: '3800',
+        ivacompra: '8%',
+        precio_venta:'4200'
+    },
+    {
+        codigo_producto: '3',
+        nombre_producto: 'Producto #3',
+        nitproveedor: '123',
+        precio_compra: '5000',
+        ivacompra: '8%',
+        precio_venta:'6000'
+    },
+]
---
+++ src/components/Products/ProductsList.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+//import { Link } from 'react-router-dom'
+import './Products.css'
+import { ProductsData } from './ProductsData'
+
+export const ProductsList = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        setProducts(ProductsData)
+        console.log(products)
+    }, [])
+
+    return (
+        <div className="content">
+            <div className="container">
+                <h2 className="mb-5 page-name text-center">Productos</h2>
+
+                <div className="table-responsive custom-table-responsive">
+
+                    <table className="table custom-table">
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Nombre</th>
+                                <th>Proveedor</th>
+                                <th>Precio de Compra</th>
+                                <th>Iva</th>
+                                <th>Precio de Venta</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map(product => (
+                                <Product
+                                    product={product}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+const Product = ({ product }) => {
+    return (<tr scope="row">
+        <td>{product.codigo_producto}</td>
+        <td>{product.nombre_producto}</td>
+        <td>{product.nitproveedor}</td>
+        <td>{product.precio_compra}</td>
+        <td>{product.ivacompra}</td>
+        <td>{product.precio_venta}</td>
+    </tr>)
+}
