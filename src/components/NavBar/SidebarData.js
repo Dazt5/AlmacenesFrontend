db7ae1af6b111a02af13eb8b494e8c2f0082bb66
David Alvarez <david.alvarez@valid.com>
2021-11-18 22:51:14 -0500

    ft: added NavBar component

---
+++ src/components/NavBar/SidebarData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import * as FaIcons from 'react-icons/fa'
+//import * as AiIcons from 'react-icons/ai'
+//import * as IoIcons from 'react-icons/io'
+
+
+export const SidebarData = [
+    {
+        title: 'Products',
+        path: '/products',
+        icon: <FaIcons.FaCartPlus />,
+        cName: 'nav-text'
+    },
+]
