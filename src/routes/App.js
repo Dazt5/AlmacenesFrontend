94c370763e9ccb0d869020b76f8a56a65514d7ab
David Alvarez <david.alvarez@valid.com>
2021-11-18 22:53:12 -0500

    ft: added new route to product component, edited route to Login element and added global styles

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
-import { Login } from '../components/Login/Login';
+import Login from '../pages/Login';
 
 import {
     BrowserRouter as Router,
     Routes, Route
 } from 'react-router-dom';
 
+import './App.css';
+
+import Products from '../pages/Products';
+
 const App = () => {
     return (
         <Router>
+            
             <Routes>
-                <Route path="/" element={<Login/>} />
+                <Route path="/" exact element={<Login />} />
+                <Route path="/products" element={<Products />} />
             </Routes>
         </Router>
     )
