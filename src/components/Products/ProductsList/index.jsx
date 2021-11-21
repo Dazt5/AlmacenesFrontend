09d02e41995bb6600b3f9d445eb82da2beb70b7f
David Alvarez <david.alvarez@valid.com>
2021-11-20 23:36:01 -0500

    ft: added buttons to delete or edit a product, added http request to getProducts or delete a product, handled http errors

--- src/components/Products/ProductsList/index.jsx
+++ src/components/Products/ProductsList/index.jsx
@@ -1,44 +1,84 @@
-import React, { useState, useEffect } from 'react'
-//import { Link } from 'react-router-dom'
-import './Products.css'
-import { ProductsData } from './ProductsData'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Products.css';
+import * as BsIcons from 'react-icons/bs'
+import { api, microservicesUri } from '../../../config/axiosConfig';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import Spinner from '../../components/Spinner/Spinner';
+import { HttpRequestOnActionHandler } from '../../../config/httpHandlers';
+import { useNavigate } from 'react-router';
+import Swal from 'sweetalert2';
 
 export const ProductsList = () => {
 
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const getProductos = async () => {
+        setLoading(true);
+        try {
+            const { data } = await api.get(microservicesUri.products);
+            setProducts(data);
+            setLoading(false);
+        } catch (error) {
+            HttpRequestOnActionHandler(error, navigate)
+        } finally {
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
 
-        setProducts(ProductsData)
-        console.log(products)
-    }, [products])
+        getProductos();
+        //eslint-disable-next-line
+    }, [])
 
     return (
         <div className="content">
             <div className="container">
-                <h2 className="mb-5 page-name text-center">Productos</h2>
 
-                <div className="table-responsive custom-table-responsive">
+                {loading && <Spinner />}
 
-                    <table className="table custom-table">
-                        <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Nombre</th>
-                                <th>Proveedor</th>
-                                <th>Precio de Compra</th>
-                                <th>Iva</th>
-                                <th>Precio de Venta</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(product => (
-                                <Product
-                                    product={product}
-                                />
-                            ))}
-                        </tbody>
-                    </table>
+                {products.length <= 0 && !loading ?
+                    <ErrorMessage
+                        message={"No existen productos registrados"}
+                    />
+                    :
+
+                    <div className="table-responsive custom-table-responsive">
+
+                        <table className="table custom-table">
+                            <thead>
+                                <tr>
+                                    <th>Código</th>
+                                    <th>Nombre</th>
+                                    <th>Proveedor</th>
+                                    <th>Precio de Compra</th>
+                                    <th>Iva</th>
+                                    <th>Precio de Venta</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.map(product => (
+                                    <Product
+                                        key={product.codigo_producto}
+                                        product={product}
+                                        getProductos={getProductos}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                }
+
+
+                <div className="action-buttons">
+                    <Link to="/product/new">
+                        <h3 className="btn btn-dark text-center"> Crear produto</h3>
+                    </Link>
                 </div>
             </div>
         </div>
@@ -46,14 +86,54 @@ export const ProductsList = () => {
 }
 
 
-const Product = ({ product }) => {
-    return (<tr>
-        <td>{product.codigo_producto}</td>
-        <td>{product.nombre_producto}</td>
-        <td>{product.nitproveedor}</td>
-        <td>{product.precio_compra}</td>
-        <td>{product.ivacompra}</td>
-        <td>{product.precio_venta}</td>
-    </tr>
+const Product = ({ product, getProductos }) => {
+
+    const navigate = useNavigate();
+
+    const deleteProduct = async () => {
+
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Esta operación no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminalo.'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    const { data } = await api.delete(`${microservicesUri.products}${product.codigo_producto}`)
+
+                    Swal.fire(
+                        'Eliminado!',
+                        data.message,
+                        'success'
+                    );
+                    getProductos();
+                } catch (error) {
+                    HttpRequestOnActionHandler(error, navigate)
+                }
+            }
+        });
+    }
+
+    return (
+        <tr>
+            <td>{product.codigo_producto}</td>
+            <td>{product.nombre_producto}</td>
+            <td>{product.nitproveedor}</td>
+            <td>{product.precio_compra}</td>
+            <td>{product.ivacompra}</td>
+            <td>{product.precio_venta}</td>
+            <td>
+                <div className="table-buttons">
+                    <BsIcons.BsTrashFill className="trash-icon" onClick={deleteProduct} />
+                    <Link to={`/product/edit/${product.codigo_producto}`}>
+                        <BsIcons.BsPencilSquare className="pencil-icon" />
+                    </Link>
+                </div>
+            </td>
+        </tr>
     )
 }
