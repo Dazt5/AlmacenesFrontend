86f43c9bf4931c551668450e7fcf8773a6bcffc3
David Alvarez <david.alvarez@valid.com>
2021-11-20 23:38:04 -0500

    ft: added product list page

--- src/pages/Product/Products.js
+++ src/pages/Product/Products.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { NavBar } from '../Components/NavBar/NavBar';
-import { ProductsList } from '../Components/Products/ProductsList';
+import { NavBar } from '../../Components/NavBar/NavBar';
+import { ProductsList } from '../../Components/Products/ProductsList';
 
 function Products() {
     return (
         <>
             <NavBar />
+            <h2 className="mb-5 page-name text-center">Productos</h2>
             <div className='main-page'>
                 <ProductsList />
             </div>
