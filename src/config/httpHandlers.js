6863f7a2abf16d8ed4928cc34a8b7b26030017b0
David Alvarez <david.alvarez@valid.com>
2021-12-04 10:44:00 -0500

    ft: removed unused handler and added validation in case it wasn't http error

--- src/config/httpHandlers.js
+++ src/config/httpHandlers.js
@@ -1,6 +1,14 @@
 import Swal from "sweetalert2";
 
 export const HttpRequestOnActionHandler = (error, navigate) => {
+    if (!error.request.status) {
+        Swal.fire(
+            'Error desconocido',
+            'Ha ocurrido un error desconocido intente de nuevo',
+            'error'
+        )
+    }
+
     const httpStatusCode = error.request.status;
 
     if (httpStatusCode === 0) {
@@ -19,43 +27,6 @@ export const HttpRequestOnActionHandler = (error, navigate) => {
         const { message } = error.response.data;
 
         if (httpStatusCode === 403 || httpStatusCode === 401) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Sesión caducada',
-                text: 'Su sesión ha caducado, inicie sesión de nuevo',
-            });
-
-            navigate("/")
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error.',
-                text: message,
-            })
-        }
-    }
-}
-
-//TODO: MAKE A SIGNATURE TO ERROR 400 IN THE BACKEND SERVICES
-export const HttpRequestOnloadHandler = (error, navigate) => {
-    const httpStatusCode = error.request.status;
-
-    if (httpStatusCode === 0) {
-        Swal.fire(
-            'Error desconocido',
-            'Ha ocurrido un error de conexión con el servidor',
-            'error'
-        )
-    } else if (httpStatusCode >= 500 && httpStatusCode <= 511) {
-        Swal.fire(
-            'Error inesperado',
-            'Ha ocurrido un error inesperado',
-            'error'
-        )
-    } else if (httpStatusCode >= 400 && httpStatusCode <= 499) {
-        const { message } = error.response.data;
-        
-        if (httpStatusCode === 403 ||httpStatusCode === 401) {
             Swal.fire({
                 icon: 'error',
                 title: 'Sesión caducada',
